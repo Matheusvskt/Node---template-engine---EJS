b49f924ea3c407a95d7b134ab8f1b520e9c38d2d
matheusvskt <matheusvieira99@gmail.com>
2022-01-12 18:09:27 -0300

    using for

--- index.js
+++ index.js
@@ -3,18 +3,32 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-let user = {
-    name:"Math Vieira",
-    phone:"(23)4545454/5445"
-}
+let users = [{
+        id: 0,
+        name: "Matheus",
+        phone: "(32)545454-5555"
+    },
+    {
+        id: 1,
+        name: "João",
+        phone: "(32)545454-6666"
+    },
+    {
+        id: 2,
+        name: "Tog",
+        phone: "(32)545455-85"
+    },
+]
+
+
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "ejs"),
 
-app.get("/", (req, res) => {
+    app.get("/", (req, res) => {
 
-    res.render('user.ejs', user)
+        res.render('user.ejs', {users})
 
-})
+    })
 
 const PORT = 5000;
 app.listen(PORT, () => {
